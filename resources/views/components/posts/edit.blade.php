d80317d00641704552b4bbc07d43940edf497d7d
Fery Eka Mahendra <Ferrh.mahendra@gmail.com>
2025-12-12 19:01:52 +0700

    minor change

--- resources/views/components/posts/edit.blade.php
+++ resources/views/components/posts/edit.blade.php
@@ -33,8 +33,7 @@
         </div>
         <div class="mb4">
             <label for="content" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Content</label>
-            <textarea id="content" name="content" rows="4" class="hidden @error('content')
-                 bg-danger-soft placeholder:text-fg-danger-strong @enderror block p-2.5 w-full text-sm text-gray-900 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Write your content here">
+            <textarea id="content" name="content" rows="4" class="hidden @error('content') bg-danger-soft placeholder:text-fg-danger-strong @enderror block p-2.5 w-full text-sm text-gray-900 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Write your content here">
                  {{ old('content') ?? $post->content }}
             </textarea>
             <div id="editor">{!! old('content', $post->content) !!}</div>
